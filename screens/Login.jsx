ce4047060281db1e89d1b1d29322af9cf28dba95
Jacek <69013954+Jacux@users.noreply.github.com>
2024-11-06 12:41:04 +0100

    Update Login.jsx

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,11 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button} from "react-native";
+import {useState} from 'react'
+import { useAuth } from "./context/authContext";
 
 export default function Login() {
+    const { onLogin } = useAuth()
+    const [email, setEmail] = useState('')
+    const [passsword, setPassword] = useState('')
+
+    const login = () => {
+        if (passsword !== '' || email !== '') {
+        onLogin(email, passsword)
+        }
+    }
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <StatusBar style="auto"></StatusBar>
+        {/*Tu bedzie logo*/}
+      <Text>EcoBoost</Text>
+        <TextInput placeHolder='Email'></TextInput>
+        <TextInput placeHolder='Hasło'></TextInput>
+        <Button onPress={login} title='Zaloguj się'></Button>
+      
     </View>
   );
 }
